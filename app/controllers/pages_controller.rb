5fc6ad3d6fb950545c90904160d7bc72b25f4175
gabriel roux <rouxgab81@gmail.com>
2022-11-11 16:28:30 -0500

    can see all the hash

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,9 @@
 class PagesController < ApplicationController
   def home
     if params[:query].present?
-      @products =  Openfoodfacts::Product.search(params[:query], locale: 'world', page_size: 10)
+      @products =  Openfoodfacts::Product.search(params[:query], locale: 'world', page_size: 3)
     else
-      @products =  Openfoodfacts::Product.search("chocolat", locale: 'world', page_size: 10)
+      @products =  Openfoodfacts::Product.search("chocolat", locale: 'world', page_size: 3)
     end
   end
 end
